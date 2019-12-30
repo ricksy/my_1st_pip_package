5b60490246094a8e54fdf134d6e972bf2d56316e
ahmed <786869+ricksy@users.noreply.github.com>
2019-12-30 21:05:00 +0100

    added setup script

---
+++ setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+
+     name='rev',  
+
+     version='0.1',
+
+     scripts=['rev'] ,
+
+     author="Ahmed",
+
+     author_email="ricksy@mailbox.org",
+
+     description="A reverse text utility package",
+
+     long_description=long_description,
+
+   long_description_content_type="text/markdown",
+
+     url="https://github.com/ricksy",
+
+     packages=setuptools.find_packages(),
+
+     classifiers=[
+
+         "Programming Language :: Python :: 3",
+
+         "License :: OSI Approved :: MIT License",
+
+         "Operating System :: OS Independent",
+
+     ],
+
+ )
+
